c32a7bc2e8f5a7b699a44a294c54fce071232dfb
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-12-16 12:18:42 -0800

    app connected to api

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from '../Book';
 import AddBook from '../AddNewBook';
+import { getBooks } from '../../redux/books/bookSlice';
 
 const Books = () => {
-  const booksList = useSelector((state) => state.bookReducer);
+  const booksList = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  if (booksList.length === 0) {
+    return 'loading...';
+  }
+
   return (
     <div className="container">
       <div className="booksContainer">
-        {booksList.map((book) => (
-          <Book key={book.id} book={book} />
+        {Object.entries(booksList[0]).map(([k, v]) => (
+          <Book key={k} book={v[0]} id={k} />
         ))}
       </div>
       <AddBook />
--- src/redux/configureStore/configuration.js
+++ src/redux/configureStore/configuration.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from '../books/books';
+// import bookReducer from '../books/books';
+import books from '../books/bookSlice';
 import categoriesReducer from '../categories/categories';
 
 const store = configureStore({
   reducer: {
-    bookReducer,
+    books,
     categoriesReducer,
   },
 });
